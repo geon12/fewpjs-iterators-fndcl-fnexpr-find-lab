3f0037f7eb376848aa9b6ed02bc4c903711d11f7
Cameron <camcreel@gmail.com>
2021-04-04 14:33:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(winLossRecord) {
+    const superBowlRecord = winLossRecord.find(superBowl => superBowl.result == "W")
+    if (superBowlRecord === undefined){
+        return superBowlRecord;
+    }
+    else {
+        return superBowlRecord.year;
+    }
+}
